dfaef7e3300161fe43dc8911ad19cc17977b4ba2
Candace Robbins <canrob0522@gmail.com>
2022-05-31 17:09:11 -0500

    added eslint file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './Components/Nav';
 import Footer from './Components/Footer';
 import './styles/app.css';
+import Assets from './Assets/emma-harper-j3CjZYckM88-unsplash.jpg'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       </header>
       <section>
             <div class="container"> 
-                <img className="Container-background-image" src="./Assets/emma-harper-j3CjZYckM88-unsplash.jpg" title="Serenity on a lake" alt="Serenity on a lake" />
+                <img className="Container-background-image" src={Assets} title="Serenity on a lake" alt="Serenity on a lake" />
                 <h2 class="in-picture">Be Strong; Be Courageous</h2>
             </div>
         </section>
