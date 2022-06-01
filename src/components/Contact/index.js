b11d1d8658e0c539de69cc27537eee5be4a89ab1
Candace Robbins <canrob0522@gmail.com>
2022-06-01 16:18:24 -0500

    added form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,14 +6,21 @@ export default function Contact() {
       <div className="container">
         <section>
         <h2 id="Contact-Me">Contact Me</h2>
-        <nav>
-            <ul>
-                <li><a href="mailto:canrob0522@gmail.com">Email</a></li>
-                <li><a href="https://github.com/CanRo2B">GitHub</a></li>
-                <li><a id="Resume" href="https://docs.google.com/document/d/11xnTInxgp1RlQknzUAW5cJQgwXmuyQFAr6vwY79_HgI/export?format=pdf">Resume</a></li>
-                <li><a href="https://www.linkedin.com/in/candace-robbins-364b95232?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BQLmHDRHTTyCvrefhJbYFkw%3D%3D">LinkedIn</a></li>
-            </ul>
-        </nav>
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input 
+              name="name" type="text" className="contactForm" placeholder="Please insert your name" id="name" />
+          <br />
+          <label htmlFor="email">Email:</label>
+          <input 
+             name="email" type="text" className="contactForm" placeholder="Please insert your email address" id="email" />
+          <br />
+          <label htmlFor="message">Message:</label>
+          <input 
+             name="message" type="text" className="contactForm" placeholder="Optional Message" id="message" />
+          <br />
+          <button onClick="" className="btn btn-primary" type="submit">Submit</button>
+        </form>
         </section>
       </div>
       );
